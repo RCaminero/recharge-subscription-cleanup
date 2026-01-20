1a7b28e14fbaafbc18bc5a901742d6b336de7947
Rcaminero <nayejudo@gmail.com>
2026-01-20 14:59:26 -0400

    feat: change SP comments

--- recharge/payment.py
+++ recharge/payment.py
@@ -1,6 +1,6 @@
 def is_payment_valid(subscription):
     """
-    Define si una suscripción tiene pago válido
+    Define if the susbcription has a valid payment
     """
     if subscription.get("max_retries_reached"):
         return False
